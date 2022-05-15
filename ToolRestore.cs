5ce7574e8abba9096dfacf804d354cac812dc04d
Rotari Pavel <zonex5@mail.ru>
2022-05-15 22:22:13 +0300

    Worked dump. Remove event OnNextClick

---
+++ ToolRestore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostgreSQL_Restore_DB
+{
+    public class ToolRestore : ITool
+    {
+        private ToolDatabaseUI toolUI;
+
+        public IToolRuner ToolRuner { get; set; }
+
+        public string Caption { get => "Database Restore Tool"; }
+
+        public bool ButtonNextEnable { get => true; }
+
+        public UserControl MainContainer
+        {
+            get
+            {
+                if (toolUI == null) toolUI = new ToolDatabaseUI(this);
+                return toolUI;
+            }
+        }
+
+        public ToolRestore(IToolRuner toolRuner)
+        {
+            ToolRuner = toolRuner;
+
+            toolRuner.OnClickNext += ToolRuner_OnClickNext;
+        }
+
+        private void ToolRuner_OnClickNext()
+        {
+
+        }
+    }
+}
